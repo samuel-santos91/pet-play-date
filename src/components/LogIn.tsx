a0c6bed676658e988eb31253db794d5f98dff803
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-12-11 18:11:00 +1100

    creates log in and sign in components and their forms

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,5 +1,24 @@
 const LogIn = () => {
-  return <p>log in</p>;
+  return (
+    <>
+      <form>
+        <h2>LOG IN</h2>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input className="border-solid border-2 border-black" type="text" id="username"/>
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            className="border-solid border-2 border-black"
+            type="password"
+            id="password"
+          />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default LogIn;
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,5 +1,46 @@
 const SignUp = () => {
-  return <p>sign up</p>;
+  return (
+    <>
+      <form>
+        <h2>SIGN UP</h2>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            className="border-solid border-2 border-black"
+            type="text"
+            id="username"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            className="border-solid border-2 border-black"
+            type="email"
+            id="email"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            className="border-solid border-2 border-black"
+            type="password"
+            id="password"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            className="border-solid border-2 border-black"
+            type="password"
+            id="confirmPassword"
+          />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default SignUp;
