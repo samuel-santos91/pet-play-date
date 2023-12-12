2bf7c7640fc061e71722d7d6edcec1af7c2ad948
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-12-12 16:22:43 +1100

    adds useForm and form submission handling

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,11 +1,29 @@
+import { useForm } from "react-hook-form";
+
+interface LogInInput {
+  username: string;
+  password: string;
+}
+
 const LogIn = () => {
+  const { register, handleSubmit } = useForm<LogInInput>();
+
+  const formSubmit = (data: LogInInput) => {
+    console.log(data)
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(formSubmit)}>
         <h2>LOG IN</h2>
         <div>
           <label htmlFor="username">Username</label>
-          <input className="border-solid border-2 border-black" type="text" id="username"/>
+          <input
+            className="border-solid border-2 border-black"
+            type="text"
+            id="username"
+            {...register("username")}
+          />
         </div>
 
         <div>
@@ -14,8 +32,10 @@ const LogIn = () => {
             className="border-solid border-2 border-black"
             type="password"
             id="password"
+            {...register("password")}
           />
         </div>
+        <button className="border-solid border-2 border-black">SUBMIT</button>
       </form>
     </>
   );
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,21 @@
+import { useForm } from "react-hook-form";
+
+interface SignUpInput {
+  username: string;
+  email: string;
+  password: string;
+}
+
 const SignUp = () => {
+  const { register, handleSubmit } = useForm<SignUpInput>();
+
+  const formSubmit = (data: SignUpInput) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(formSubmit)}>
         <h2>SIGN UP</h2>
         <div>
           <label htmlFor="username">Username</label>
@@ -9,6 +23,7 @@ const SignUp = () => {
             className="border-solid border-2 border-black"
             type="text"
             id="username"
+            {...register("username")}
           />
         </div>
 
@@ -18,6 +33,7 @@ const SignUp = () => {
             className="border-solid border-2 border-black"
             type="email"
             id="email"
+            {...register("email")}
           />
         </div>
 
@@ -27,17 +43,20 @@ const SignUp = () => {
             className="border-solid border-2 border-black"
             type="password"
             id="password"
+            {...register("password")}
           />
         </div>
 
-        <div>
+        {/* <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
           <input
             className="border-solid border-2 border-black"
             type="password"
             id="confirmPassword"
           />
-        </div>
+        </div> */}
+
+        <button className="border-solid border-2 border-black">SUBMIT</button>
       </form>
     </>
   );
